1e5398e96f3cb65731a67fffb366dd469a523833
eljebrounisiufian <42852578+jebrouni@users.noreply.github.com>
2020-06-15 06:44:11 +0100

    nouvelle fichier

--- src/main/java/com/example/web/EtudiantController.java
+++ src/main/java/com/example/web/EtudiantController.java
@@ -4,20 +4,23 @@ package com.example.web;
 
 
 import java.util.List;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Date;
 
 
 import javax.validation.Valid;
 
-
-
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartProperties;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -25,6 +28,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.dao.EtudiantRepository;
@@ -64,26 +68,72 @@ public class EtudiantController {
 		return "FormEtudiant";
 	}
 	@RequestMapping(value="/SaveEtudiant", method=RequestMethod.POST)
+	//valid pour entre les formation des etudiant 
+	//bindingResult for Error les inputs empty
+	//MultipartFile for photo is correct or not
 	public String  save(@Valid Etudiant et,BindingResult bindingResult,
 			@RequestParam(name="picture") MultipartFile file) throws Exception, IOException {
 		if(bindingResult.hasErrors()) {
 		
 			return "FormEtudiant";
 		}
+		//enregester le photo qui envoyer par le clients
 		if(!(file.isEmpty())) {
 			et.setPhoto(file.getOriginalFilename());
 		}
 		etudiantRepository.save(et);
 		if(!(file.isEmpty())) {
-			
+			//save photo by id
      	    et.setPhoto(file.getOriginalFilename());
      		file.transferTo(new File(imageDir+et.getId()));
 		}
 
-			
-		
-		
-		
 		return "redirect:Index";
+	}
+	/////////////////////////////////////////////// UPDATE
+	@RequestMapping(value="/UpdateEtudiant", method=RequestMethod.POST)
+	//valid pour entre les formation des etudiant 
+	//bindingResult for Error les inputs empty
+	//MultipartFile for photo is correct or not
+	public String  update(@Valid Etudiant et,BindingResult bindingResult,
+			@RequestParam(name="picture") MultipartFile file) throws Exception, IOException {
+		if(bindingResult.hasErrors()) {
+		
+			return "EditEtudiant";
+		}
+		//enregester le photo qui envoyer par le clients
+		if(!(file.isEmpty())) {
+			et.setPhoto(file.getOriginalFilename());
+		}
+		etudiantRepository.save(et);
+		if(!(file.isEmpty())) {
+			//save photo by id
+     	    et.setPhoto(file.getOriginalFilename());
+     		file.transferTo(new File(imageDir+et.getId()));
+		}
+
+		return "redirect:Index";
+	}
+	@RequestMapping(value="/getPhoto",produces=MediaType.IMAGE_JPEG_VALUE)
+	@ResponseBody
+	public byte[] getPhoto(Long id) throws IOException {
+		File f=new File(imageDir+id);
+		return org.apache.commons.io.IOUtils.toByteArray(new FileInputStream(f));
+				
+		
+	}
+	@RequestMapping(value="/supprimer")
+	public String supprimer(Long id) {
+		 etudiantRepository.deleteById(id);
+		 return "redirect:Index";
+		
+	}
+	@RequestMapping(value="/edit")
+	public String edit(Long id,Model model) {
+		
+		 Etudiant et=etudiantRepository.getOne(id);
+		 model.addAttribute("etudiant", et);
+		 return "EditEtudiant";
+		
 	}
 	}
